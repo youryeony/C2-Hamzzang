453568f4512f9cf13cefea66551289954e7b34db
Yeony <yeony.ndy@gmail.com>
2025-04-21 19:59:12 +0900

    Initial Commit

---
+++ miniHamzzang/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  miniHamzzang
+//
+//  Created by Doyeon Nam on 4/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ miniHamzzang/miniHamzzangApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniHamzzangApp.swift
+//  miniHamzzang
+//
+//  Created by Doyeon Nam on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct miniHamzzangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
