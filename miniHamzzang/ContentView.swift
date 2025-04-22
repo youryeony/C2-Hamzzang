989101609971a3c83c6ebb223f9a021b4a7a9697
Yeony <yeony.ndy@gmail.com>
2025-04-23 02:19:52 +0900

    Initializes SwiftData integration for Hamzzang data
    Sets up SwiftData to manage and persist `Hamzzang` data.
    This includes creating a ModelContainer in the `ContentView_Previews`
    and passing the `Hamzzang` instance using `@State` and `@Binding`
    to `HamzzangMainView` and `HamzzangListView` respectively.
    This allows the app to store and retrieve data related to Hamzzangs.

--- miniHamzzang/ContentView.swift
+++ miniHamzzang/ContentView.swift
@@ -1,17 +1,21 @@
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    // SwiftData 모델 인스턴스 생성(저장 가능한 데이터 모델)
+    @State private var hamzzang = Hamzzang(name: "") // 초기값은 빈칸 햄짱이
+
     @State private var selection = 1
 
     var body: some View {
         TabView(selection: $selection) {
-            HamzzangMainView(hamzzang: Hamzzang())
+            HamzzangMainView(hamzzang: hamzzang) // Bindable로 넘겨줄 거
                 .tabItem {
                     Image(selection == 1 ? "hamshill" : "hamshillbk")
                 }
                 .tag(1)
 
-            HamzzangListView()
+            HamzzangListView(selectedHamzzang: $hamzzang) // @Binding이니까 $ 붙임
                 .tabItem {
                     Image(selection == 2 ? "hamface" : "hamfacebk")
                 }
@@ -28,5 +32,10 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    let container = try! ModelContainer(
+        for: Hamzzang.self, Note.self,
+        configurations: ModelConfiguration(isStoredInMemoryOnly: true)
+    )
+    return ContentView()
+        .modelContainer(container)
 }
